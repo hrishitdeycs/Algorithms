346b190621af5edb4b7a8ecddfbf1f6843dd203f
Hrishit Dey <hrishitdey@gmail.com>
2026-05-06 11:01:20 +0530

    to determine a minimum spanning tree of a graph using the Prim’s  algorithm.

---
+++ prims_algorithm.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+const int V = 5;
+const int INF = 999999;
+int minKey(int key[], bool mstSet[]) {
+    int min = INF, min_index;
+    for (int v = 0; v < V; v++)
+        if (!mstSet[v] && key[v] < min)
+            min = key[v], min_index = v;
+    return min_index;
+}
+void primMST(int graph[V][V]) {
+    int parent[V], key[V];
+    bool mstSet[V];
+    for (int i = 0; i < V; i++)
+        key[i] = INF, mstSet[i] = false;
+    key[0] = 0;
+    parent[0] = -1;
+    for (int count = 0; count < V - 1; count++) {
+        int u = minKey(key, mstSet);
+        mstSet[u] = true;
+        for (int v = 0; v < V; v++)
+            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v])
+                parent[v] = u, key[v] = graph[u][v];
+    }
+    for (int i = 1; i < V; i++)
+        cout << parent[i] << " - " << i << " : " << graph[i][parent[i]] << endl;
+}
+int main() {
+    int graph[V][V] = {
+        {0, 2, 0, 6, 0},
+        {2, 0, 3, 8, 5},
+        {0, 3, 0, 0, 7},
+        {6, 8, 0, 0, 9},
+        {0, 5, 7, 9, 0}
+    };
+    primMST(graph);
+    return 0;
+}
